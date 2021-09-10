8d38ea5634b16fa5c7cdae6dc7b145f777916cff
Aidan Yates <ayates67@ccs.local>
2021-09-10 10:38:53 -0400

    fixed rgb value for hello

--- hello.py
+++ hello.py
@@ -7,11 +7,11 @@ dot.brightness = 0.1
 
 while True:
     print("Make it red!")
-    dot.fill((0, 0, 255))
+    dot.fill((255, 0, 0))
     time.sleep(0.5)
     print("Make it green!")
-    dot.fill((0, 0, 255))
+    dot.fill((0, 255, 0))
     time.sleep(0.5)
-    print("Make it blue!")
-    dot.fill((0, 0, 255))
+    print("Make it cyan!")
+    dot.fill((0, 128, 128))
     time.sleep(0.5)
